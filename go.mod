1afbd3eb8bcccedefe4df71ccea246b49a0ee28f
kwonkwonn <ggiicchh48@gmail.com>
2025-07-11 06:54:17 +0000

    loading ovn-nb schema done

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/ovn-org/libovsdb v0.7.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
@@ -23,6 +24,7 @@ require (
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
 	initialize "github.com/kwonkwonn/ovn-go-cms/initialize"
 )
@@ -14,9 +16,43 @@ func main(){
 	if err != nil {
 		log.Fatalf("Failed to initialize OVN client: %v", err)
 	}
-
 	ovnClient.Connect(context.Background())
+	ovnClient.MonitorAll(context.Background())
+	ops, err := ovnClient.Create(&initialize.Logical_Switch{
+		Name: "foo",
+	})
+	if err!=nil{
+		fmt.Println(err)
+	}
+	
+	_,err = ovnClient.Transact(context.Background(),ops...)
+	if(err!=nil){
+		fmt.Println(err)
+	}
+
+	// OVN이 내부적으로 동기화할 시간을 줍니다.
+// 이는 임시 방편이며, 실제로는 waitForCacheConsistent가 이 역할을 해야 합니다.
+time.Sleep(2 * time.Second) // 2초 대기
+
+	ls := &[]initialize.Logical_Switch{}
+	// test := &initialize.Logical_Switch{Name:"foo"}
+	ovnClient.List(context.Background(), ls )
+
+	time.Sleep(2 * time.Second) // 2초 대기
+	fmt.Println("List of logical devices:",ls)
 
 
+	for i, _ := range *ls{
+		ovs_to_del:=&initialize.Logical_Switch{UUID: (*ls)[i].UUID}
+		ops,_:= ovnClient.Where(ovs_to_del).Delete()
+		ovnClient.Transact(context.Background(),ops...)
+	}
+
+	ls = &[]initialize.Logical_Switch{}
+	// test := &initialize.Logical_Switch{Name:"foo"}
+	ovnClient.List(context.Background(), ls )
+
+	time.Sleep(2 * time.Second) // 2초 대기
+	fmt.Println("List of logical devices:",ls)
 	select{}
 }
