f9ad3647f41e164bbe7917791009319d58981ca1
kwonkwonn <ggiicchh48@gmail.com>
2025-08-04 20:12:08 +0900

    merging conflict with old main resolved

--- main.go
+++ main.go
@@ -28,23 +28,12 @@ func main(){
 	Operator.ExternSwitchs = make(map[string]*externalmodel.ExternSwitch,0)
 
 	Operator.InitializeLogicalDevices()
-<<<<<<< HEAD
 	if len(Operator.ExternRouters) == 0 && len(Operator.ExternSwitchs) == 0 {
 		err:= Operator.InitialSetting()
 		if err != nil {
 			panic("initialize error: " + err.Error())
 		}
 	}
-=======
-	if len(Operator.ExternRouters )==0 && len(Operator.ExternSwitchs) == 0 {
-		Operator.InitialSetting()
-	}
-	fmt.Println(Operator.ExternRouters)
-	fmt.Println(Operator.ExternSwitchs)
-//	초기화 조건 추가
-// 
-// 	// Chassis 초기화
->>>>>>> 26709d0995a655a4792d74bf3071920726dd1ca1
 
 	fmt.Println("ExternRouters: ", Operator.ExternRouters)
 	fmt.Println("ExternSwitchs: ", Operator.ExternSwitchs)
@@ -53,12 +42,6 @@ func main(){
 
 	}
 
-<<<<<<< HEAD
-    fmt.Println("asfsfidsaofbadsfoisabfsodifsbfa", portss)
-    fmt.Println("zxvavasdvdsas", Operator.ExternRouters[string(operation.ROUTER)].SubNetworks)	
-=======
-
->>>>>>> 26709d0995a655a4792d74bf3071920726dd1ca1
 
 
 	server.InitServer(8081,handler)
